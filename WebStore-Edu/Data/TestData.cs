4c2b99ff6b839ea929dac67ba9b1310182f71141
Денис <dultsevdv@gmail.com>
2021-12-17 03:11:28 +0500

    Тестовые данные
    Список сотрудников

---
+++ WebStore-Edu/Data/TestData.cs
@@ -0,0 +1,37 @@
+﻿using WebStore_Edu.Models;
+
+namespace WebStore_Edu.Data
+{
+    /// <summary> Тестовые данные </summary>
+    public static class TestData
+    {
+        /// <summary> Список сотрудников </summary>
+        public static List<Employee> Employees => new()
+        {
+            new()
+            {
+                FirstName = "Иван",
+                Patronymic = "Васильевич",
+                LastName = "Грозный",
+                Birthday = DateTime.Parse("15.10.1957"),
+                Position = "Начальник Всея Руси"
+            },
+            new()
+            {
+                FirstName = "Василий",
+                Patronymic = "Петрович",
+                LastName = "Васильев",
+                Birthday = DateTime.Parse("03.03.1980"),
+                Position = "Менеджер"
+            },
+            new()
+            {
+                FirstName = "Денис",
+                Patronymic = "Владимирович",
+                LastName = "Дульцев",
+                Birthday = DateTime.Parse("18.08.1988"),
+                Position = "Студент GB"
+            }
+        };
+    }
+}
