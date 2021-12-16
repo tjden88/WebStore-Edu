4622aa09b9f7ef8cce64d67398cdb8371da6567c
Денис <dultsevdv@gmail.com>
2021-12-17 02:22:42 +0500

    Начальная конфигурация приложения

--- WebStore-Edu/Program.cs
+++ WebStore-Edu/Program.cs
@@ -1,6 +1,26 @@
+#region Построение приложения
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddControllersWithViews();
+// Вопрос: почему не services.AddMvc(); ? 
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#endregion
 
+
+#region Конвейер запросов
+
+// Отладочная страница исключений
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+app.MapGet("/", () => "Привет!");
+
+#endregion
+
+// Запуск приложения
 app.Run();
