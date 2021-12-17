425f4dc793ed48e09dfdd12028c76cddc75e00ca
Денис <dultsevdv@gmail.com>
2021-12-17 18:23:40 +0500

    Контроллер блогов

---
+++ WebStore-Edu/Controllers/BlogsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore_Edu.Controllers
+{
+    public class BlogsController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Blog() => View();
+    }
+}
