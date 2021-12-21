26832ad8af55e8c6eaa40e209758af09ea3fa602
Денис <dultsevdv@gmail.com>
2021-12-22 03:31:40 +0500

    Сервис данных сотрудников

---
+++ WebStore-Edu/Services/EmployeesData.cs
@@ -0,0 +1,54 @@
+﻿using MapsterMapper;
+using WebStore_Edu.Data;
+using WebStore_Edu.Models;
+using WebStore_Edu.Services.Interfaces;
+
+namespace WebStore_Edu.Services
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly IMapper _Mapper;
+
+        public InMemoryEmployeesData(IMapper Mapper) => _Mapper = Mapper;
+
+        public IEnumerable<Employee> GetAll() => TestData.Employees;
+
+        public Employee? GetById(int Id) => TestData.Employees.FirstOrDefault(e => e.Id == Id);
+
+        public int Add(Employee employee)
+        {
+            if (employee is null)
+                throw new ArgumentNullException(nameof(employee));
+
+            employee.Id = GetNextId();
+            TestData.Employees.Add(employee);
+            return employee.Id;
+        }
+
+        public bool Update(Employee employee)
+        {
+            if (employee is null)
+                throw new ArgumentNullException(nameof(employee));
+
+            if (GetById(employee.Id) is not { } empl) return false;
+
+            //empl = _Mapper.Map<Employee>(employee);
+
+            _Mapper.Map(employee, empl);
+
+            return true;
+        }
+
+        public bool Delete(int Id)
+        {
+            if (GetById(Id) is not { } empl) return false;
+
+            TestData.Employees.Remove(empl);
+
+            return true;
+        }
+
+        private int _CurrentMaxId = TestData.Employees.Count;
+        private int GetNextId() => ++_CurrentMaxId;
+    }
+}
---
+++ WebStore-Edu/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,17 @@
+﻿using WebStore_Edu.Models;
+
+namespace WebStore_Edu.Services.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee? GetById(int Id);
+
+        int Add(Employee employee);
+
+        bool Update(Employee employee);
+
+        bool Delete(int Id);
+    }
+}
