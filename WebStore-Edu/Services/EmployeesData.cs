fd0c0aba66ddedb01f78f5526434521172ae63a6
Денис <dultsevdv@gmail.com>
2021-12-22 10:50:50 +0500

    Логгирование сервиса сотрудников

--- WebStore-Edu/Services/EmployeesData.cs
+++ WebStore-Edu/Services/EmployeesData.cs
@@ -8,8 +8,13 @@ namespace WebStore_Edu.Services
     public class InMemoryEmployeesData : IEmployeesData
     {
         private readonly IMapper _Mapper;
+        private readonly ILogger<InMemoryEmployeesData> _Logger;
 
-        public InMemoryEmployeesData(IMapper Mapper) => _Mapper = Mapper;
+        public InMemoryEmployeesData(IMapper Mapper, ILogger<InMemoryEmployeesData> Logger)
+        {
+            _Mapper = Mapper;
+            _Logger = Logger;
+        }
 
         public IEnumerable<Employee> GetAll() => TestData.Employees;
 
@@ -22,6 +27,7 @@ namespace WebStore_Edu.Services
 
             employee.Id = GetNextId();
             TestData.Employees.Add(employee);
+            _Logger.LogInformation("Добавлен новый сотрудник: {0}", employee);
             return employee.Id;
         }
 
@@ -30,20 +36,31 @@ namespace WebStore_Edu.Services
             if (employee is null)
                 throw new ArgumentNullException(nameof(employee));
 
-            if (GetById(employee.Id) is not { } empl) return false;
+            if (GetById(employee.Id) is not { } empl)
+            {
+                _Logger.LogWarning("Попытка изменить несуществующего сотрудника с id:{0}", employee.Id);
+                return false;
+            }
 
             //empl = _Mapper.Map<Employee>(employee);
 
             _Mapper.Map(employee, empl);
+            _Logger.LogInformation("Изменён сотрудник: {0}", empl);
+
 
             return true;
         }
 
         public bool Delete(int Id)
         {
-            if (GetById(Id) is not { } empl) return false;
+            if (GetById(Id) is not { } empl)
+            {
+                _Logger.LogWarning("Попытка удалить несуществующего сотрудника с id:{0}", Id);
+                return false;
+            }
 
             TestData.Employees.Remove(empl);
+            _Logger.LogInformation("Удалён сотрудник: {0}", empl);
 
             return true;
         }
