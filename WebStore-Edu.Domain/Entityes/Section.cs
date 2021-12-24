0fb7d89db88cff76b43ba48bcca1dc1b5add9581
Денис <dultsevdv@gmail.com>
2021-12-24 19:50:24 +0500

    Классы брендов, категорий, продукта

---
+++ WebStore-Edu.Domain/Entityes/Section.cs
@@ -0,0 +1,11 @@
+﻿using WebStore_Edu.Domain.Entityes.Base;
+
+namespace WebStore_Edu.Domain.Entityes
+{
+    /// <summary> Категория товара </summary>
+    public class Section : OrderedNamedEntity
+    {
+        /// <summary>ID родительской секции</summary>
+        public int ParentId { get; set; }
+    }
+}
