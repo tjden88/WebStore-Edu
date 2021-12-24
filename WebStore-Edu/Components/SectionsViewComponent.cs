c705cad0beafe69f4cc06d4646e6a592e86315c8
Денис <dultsevdv@gmail.com>
2021-12-24 20:22:04 +0500

    Компонент категорий товаров

---
+++ WebStore-Edu/Components/SectionsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore_Edu.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
