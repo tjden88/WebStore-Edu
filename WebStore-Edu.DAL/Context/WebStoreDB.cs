b2296e3b5222aaddd7e2ee76b831d3eac1420530
Денис <dultsevdv@gmail.com>
2022-01-25 21:47:18 +0500

    Класс контекста БД

---
+++ WebStore-Edu.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore_Edu.Domain.Entityes;
+
+namespace WebStore_Edu.DAL.Context
+{
+    public class WebStoreDb : DbContext
+    {
+        public WebStoreDb(DbContextOptions<WebStoreDb> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+    }
+}
