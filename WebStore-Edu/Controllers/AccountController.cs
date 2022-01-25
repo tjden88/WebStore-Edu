d43f4235fb0fed047fea4e77b1ee7fd6cfb6fe74
Денис <dultsevdv@gmail.com>
2022-01-26 02:46:30 +0500

    Заготовка контроллера Account

---
+++ WebStore-Edu/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore_Edu.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
