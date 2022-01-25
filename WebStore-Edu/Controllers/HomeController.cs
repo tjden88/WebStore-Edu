9e0e5b6828a61c633c5531fde267456852caea0e
Денис <dultsevdv@gmail.com>
2022-01-25 20:37:34 +0500

    Восстановление работоспособности главной страницы

--- WebStore-Edu/Controllers/HomeController.cs
+++ WebStore-Edu/Controllers/HomeController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebStore_Edu.Data;
+using WebStore_Edu.Services.Interfaces;
+using WebStore_Edu.ViewModels;
 
 namespace WebStore_Edu.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index([FromServices] IProductData ProductData, [FromServices] IMapper Mapper) 
         {
+            var featuredProducts = ProductData.GetProducts().Take(6);
+            ViewBag.FeaturedProducts = Mapper.Map<IEnumerable<ProductViewModel>>(featuredProducts);
             return View();
             //return View("Index"); // Если названия в папке Views не совпадают
             // return View("~/Views/Home/Index.cshtml"); // Полный путь
