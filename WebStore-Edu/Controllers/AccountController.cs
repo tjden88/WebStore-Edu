ced905353d3b1c408316dc71982e4cf35b5205e6
Денис <dultsevdv@gmail.com>
2022-01-26 04:33:56 +0500

    Регистрация пользователя и валидация данных

--- WebStore-Edu/Controllers/AccountController.cs
+++ WebStore-Edu/Controllers/AccountController.cs
@@ -1,19 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore_Edu.Domain.Identity;
 using WebStore_Edu.ViewModels;
 
 namespace WebStore_Edu.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _UserManager = userManager;
+            _SignInManager = signInManager;
+        }
+
         public IActionResult Authorize() => View();
 
         [HttpPost]
-        public IActionResult Register(RegisterUserViewModel Model)
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
         {
             if (!ModelState.IsValid)
                 return View(Model);
 
-            return RedirectToAction("Index", "Home");
+            var user = new User()
+            {
+                UserName = Model.UserName
+            };
+
+            var registrationResult = await _UserManager.CreateAsync(user, Model.Password);
+            if (registrationResult.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, true);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registrationResult.Errors)
+            {
+                switch (error.Code)
+                {
+                    case "PasswordTooShort":
+                        ModelState.AddModelError("", "Пароль слишком короткий");
+                        break;
+                    case "PasswordRequiresUniqueChars":
+                        ModelState.AddModelError("", "Пароль дложен содержать больше разных символов");
+                        break;
+                    case "DuplicateUserName":
+                        ModelState.AddModelError("", "Такой пользователь уже зарегистрирован");
+                        break;
+
+                    default:
+                        ModelState.AddModelError("", error.Description);
+                        break;
+
+                }
+            }
+            return View(Model);
         }
 
         [HttpPost]
