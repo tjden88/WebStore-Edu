8135ab275b7607cad841ef78ac3b388fb50be72b
Денис <dultsevdv@gmail.com>
2022-01-27 12:13:51 +0500

    Сущности корзины покупок

---
+++ WebStore-Edu.Domain/Entityes/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebStore_Edu.Domain.Entityes
+{
+    public class Cart
+    {
+        public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
+
+        public int TotalCount => Items.Sum(i => i.Quantity);
+    }
+}
