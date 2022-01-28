305dfe2bc48d08c963569c7b39315c73ed616ca3
Денис <dultsevdv@gmail.com>
2022-01-28 19:41:02 +0500

    Настройка доступа в область администратора

--- WebStore-Edu/Areas/Admin/Controllers/HomeController.cs
+++ WebStore-Edu/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore_Edu.Domain.Identity;
 
 namespace WebStore_Edu.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = Role.Administrators)]
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
