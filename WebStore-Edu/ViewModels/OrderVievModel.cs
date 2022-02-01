751dcf2c1d023871772932994237d5001b8e9838
Денис <dultsevdv@gmail.com>
2022-02-02 03:49:41 +0500

    Вьюмодель заказа

---
+++ WebStore-Edu/ViewModels/OrderVievModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebStore_Edu.Domain.Identity;
+
+namespace WebStore_Edu.ViewModels
+{
+    public class OrderVievModel
+    {
+        [Microsoft.Build.Framework.Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Microsoft.Build.Framework.Required]
+        public string Address { get; set; }
+
+        public string Notes { get; set; }
+
+        User User { get; set; }
+    }
+}
