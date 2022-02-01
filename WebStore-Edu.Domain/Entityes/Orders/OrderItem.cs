d304c47407f1151f596e5a52365f52bfbc2d1ed6
Денис <dultsevdv@gmail.com>
2022-02-02 03:21:36 +0500

    Сущности заказов и составляющих заказа

---
+++ WebStore-Edu.Domain/Entityes/Orders/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore_Edu.Domain.Entityes.Orders;
+
+public class OrderItem
+{
+    [Required]
+    public Product Product { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public Order Order { get; set; }
+
+    public decimal TotalItemsPrice => Price * Quantity;
+}
