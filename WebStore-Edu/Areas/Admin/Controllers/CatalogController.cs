0980a917d834b85f16504978b01bca30f7f2ba50
Денис <dultsevdv@gmail.com>
2022-02-02 01:26:52 +0500

    Контроллер каталога товаров для админа

---
+++ WebStore-Edu/Areas/Admin/Controllers/CatalogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore_Edu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
