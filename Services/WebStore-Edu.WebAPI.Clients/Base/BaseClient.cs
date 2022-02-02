d88a94c06738f131b3e745e8d520c37a70a23f17
Денис <dultsevdv@gmail.com>
2022-02-03 02:29:16 +0500

    Проект клиентов Веб-Апи

---
+++ Services/WebStore-Edu.WebAPI.Clients/Base/BaseClient.cs
@@ -0,0 +1,15 @@
+﻿namespace WebStore_Edu.WebAPI.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected HttpClient Client;
+
+        protected string Address;
+
+        protected BaseClient(HttpClient HttpClient, string Address)
+        {
+            Client = HttpClient;
+            this.Address = Address;
+        }
+    }
+}
---
+++ Services/WebStore-Edu.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,38 @@
+﻿using WebStore_Edu.Interfaces.TestApi;
+
+namespace WebStore_Edu.WebAPI.Clients.Values
+{
+    public class ValuesClient : IValuesApiService
+    {
+
+        public IEnumerable<string> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int Id, string Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
