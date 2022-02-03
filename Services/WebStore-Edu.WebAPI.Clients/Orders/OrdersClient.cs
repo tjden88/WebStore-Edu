51b0916d9f063e7d1ff2aa7d4c2f92d13567447e
Денис <dultsevdv@gmail.com>
2022-02-03 06:04:57 +0500

    Заготовка Api заказов

---
+++ Services/WebStore-Edu.WebAPI.Clients/Orders/OrdersClient.cs
@@ -0,0 +1,30 @@
+﻿using WebStore_Edu.Domain.Entityes.Orders;
+using WebStore_Edu.Domain.Identity;
+using WebStore_Edu.Domain.ViewModels;
+using WebStore_Edu.Interfaces.Services;
+using WebStore_Edu.WebAPI.Clients.Base;
+
+namespace WebStore_Edu.WebAPI.Clients.Orders
+{
+    public class OrdersClient : BaseClient, IOrderService
+    {
+        public OrdersClient(HttpClient HttpHttp) : base(HttpHttp, "api/orders")
+        {
+        }
+
+        public Task<IEnumerable<Order>> GetUserOrdersAsync(User User, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order?> GetOrderAsync(int Id, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> CreateOrderAsync(User User, CartViewModel Cart, OrderVievModel OrderModel, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebSore-Edu.WebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore_Edu.Interfaces.Services;
+
+namespace WebSore_Edu.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _OrderService;
+
+        public OrdersController(IOrderService OrderService) => _OrderService = OrderService;
+    }
+}
