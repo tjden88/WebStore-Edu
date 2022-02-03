27c34ae5c077947f4f20591a1d18dbb7da887f7f
Денис <dultsevdv@gmail.com>
2022-02-03 17:01:53 +0500

    WPF-проект для работы с Api

---
+++ UI/WebStore-Edu.WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.DependencyInjection;
+using WebStore_Edu.Domain.DTO.Orders;
+using WebStore_Edu.Domain.ViewModels;
+using WebStore_Edu.Interfaces.Services;
+using WebStore_Edu.WebAPI.Clients.Products;
+
+namespace WebStore_Edu.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        public MainWindow()
+        {
+            var services = new ServiceCollection();
+
+            services.AddHttpClient<IProductData, ProductsClient>(cfg => cfg.BaseAddress = new("http://localhost:5001"));
+            // Mapster
+            var config = new TypeAdapterConfig();
+            config.ConfigureViewModels();
+            config.ConfigureDTOModels();
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, ServiceMapper>();
+
+            var provider = services.BuildServiceProvider();
+
+            var products = provider.GetRequiredService<IProductData>().GetProducts();
+
+            InitializeComponent();
+
+            list.ItemsSource = products;
+        }
+    }
+}
