a425db3c30dc58ad7e6ad5a3b7ff689137ff05e6
Денис <dultsevdv@gmail.com>
2022-02-03 05:18:09 +0500

    Контроллер Апи для товаров

---
+++ Services/WebStore-Edu.WebAPI.Clients/Products/ProductsClient.cs
@@ -0,0 +1,39 @@
+﻿using WebStore_Edu.Domain;
+using WebStore_Edu.Domain.Entityes;
+using WebStore_Edu.Interfaces.Services;
+using WebStore_Edu.WebAPI.Clients.Base;
+
+namespace WebStore_Edu.WebAPI.Clients.Products
+{
+    public class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(HttpClient HttpHttp) : base(HttpHttp, "api/products")
+        {
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Brand> GetBrands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter? filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product? GetProduct(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebSore-Edu.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore_Edu.Domain;
+using WebStore_Edu.Interfaces.Services;
+
+namespace WebSore_Edu.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+
+        [HttpGet("sections")]
+        public IActionResult GetSections() => Ok(_ProductData.GetSections());
+
+
+        [HttpGet("brands")]
+        public IActionResult GetBrands() => Ok(_ProductData.GetBrands());
+
+        [HttpPost]
+        public IActionResult GetProducts(ProductFilter? Filter = null) => Ok(_ProductData.GetProducts(Filter));
+
+
+        [HttpGet("{Id}")]
+        public IActionResult GetProduct(int Id)
+        {
+            var product = _ProductData.GetProduct(Id);
+            return product is null
+                ? NotFound()
+                : Ok(product);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            var success = _ProductData.Remove(Id);
+            return success ? Ok(success) : NotFound(success);
+        }
+    }
+}
