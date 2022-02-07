8506279c747e87077f197eff631f7ec70b73ac73
Денис <dultsevdv@gmail.com>
2022-02-08 02:59:36 +0500

    Заготовка Таг-хелпера активной ссылки

---
+++ UI/WebStore-Edu/TagHelpers/IsActiveLink.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore_Edu.TagHelpers;
+
+[HtmlTargetElement(Attributes = AttributeName)]
+public class IsActiveLink : TagHelper
+{
+    private const string AttributeName = "is-active-link";
+
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.RemoveAll(AttributeName);
+    }
+}
