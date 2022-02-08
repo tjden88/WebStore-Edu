b1b79cb288ecdd1a5fb686be4f6c95bb4a861ebd
Денис <dultsevdv@gmail.com>
2022-02-08 06:32:14 +0500

    Карта сайта

---
+++ UI/WebStore-Edu/Controllers/SitemapController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using WebStore_Edu.Interfaces.Services;
+
+namespace WebStore_Edu.Controllers
+{
+    public class SitemapController : ControllerBase
+    {
+        public IActionResult Index([FromServices] IProductData ProductData)
+        {
+            var nodes = new List<SitemapNode>()
+            {
+                new(Url.Action("Index", "Home")),
+                new(Url.Action("Contacts", "Home")),
+                new(Url.Action("Index", "Blogs")),
+                new(Url.Action("Blog", "Blogs")),
+                new(Url.Action("Index", "WebApi")),
+                new(Url.Action("Index", "Shop")),
+            };
+
+            nodes.AddRange(ProductData.GetSections().Select(s => new SitemapNode(Url.Action("Index", "Shop", new { SectionId = s.Id}))));
+            nodes.AddRange(ProductData.GetBrands().Select(b => new SitemapNode(Url.Action("Index", "Shop", new { BrandId = b.Id}))));
+            nodes.AddRange(ProductData.GetProducts().Select(p => new SitemapNode(Url.Action("ProductDetails", "Shop", new {p.Id}))));
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
