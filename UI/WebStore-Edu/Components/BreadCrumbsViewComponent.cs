168cef17b25572b65c337cb9d0fe47e49c8c03dc
Денис <dultsevdv@gmail.com>
2022-02-08 05:46:48 +0500

    Заготовка визуального компонента BreadCrumbs

---
+++ UI/WebStore-Edu/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore_Edu.Domain.ViewModels;
+
+namespace WebStore_Edu.Components
+{
+    public class BreadCrumbsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new BreadCrumbsViewModel();
+
+            return View(model);
+        }
+    }
+}
