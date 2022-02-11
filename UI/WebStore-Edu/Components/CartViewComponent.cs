f70f35f86d0a52dcb351576712e83d2b50344dd6
Денис <dultsevdv@gmail.com>
2022-02-11 19:46:19 +0500

    Визуальный компонент корзины в меню

---
+++ UI/WebStore-Edu/Components/CartViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore_Edu.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
