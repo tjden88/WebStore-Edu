f9cac28e9b7a114ce9cccb9c9057d607e315936f
Денис <dultsevdv@gmail.com>
2022-02-11 20:32:30 +0500

    Переделка логики отображения количества товаров в корзине

--- UI/WebStore-Edu/Components/UserMenuViewComponent.cs
+++ UI/WebStore-Edu/Components/UserMenuViewComponent.cs
@@ -5,13 +5,9 @@ namespace WebStore_Edu.Components
 {
     public class UserMenuViewComponent : ViewComponent
     {
-        private readonly ICartService _CartService;
-
-        public UserMenuViewComponent(ICartService CartService) => _CartService = CartService;
 
         public IViewComponentResult Invoke()
         {
-            ViewBag.CartCount = _CartService.ProductsCount();
             return User.Identity.IsAuthenticated
                 ? View("Authorized")
                 : View();
